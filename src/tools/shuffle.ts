611c91fcdd46380f200ce394f39910789a613b95
Anton Ovechkin <owanturist@gmail.com>
2025-03-09 12:28:35 +0100

    Assign random style.order

--- src/tools/shuffle.ts
+++ src/tools/shuffle.ts
@@ -1,4 +1,5 @@
 export function shuffle<T>(
+  random: () => number,
   array: ReadonlyArray<T>,
   start = 0,
   end = array.length - 1,
@@ -6,7 +7,7 @@ export function shuffle<T>(
   const result = array.slice()
 
   for (let source = start; source < end; source++) {
-    const target = Math.floor(Math.random() * (end - source + 1)) + source
+    const target = Math.floor(random() * (end - source + 1)) + source
     const tmp = result[source]
 
     result[source] = result[target]
